cc7780c60aaf89ca7766a58bbf74fccf4415ee42
inrongpon22 <inrongpon.phanupong2@gmail.com>
2022-10-27 16:06:40 +0700

    edited sth

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const featuresMenu = document.getElementById('drop-down-features');
 const featuresDrop = document.getElementById('features-drop');
+const hamMenu = document.getElementById('ham-menu');
+
 
 const companyMenu = document.getElementById('drop-down-company');
 const companyDrop = document.getElementById('company-drop');
 
-featuresMenu.addEventListener('mouseenter', e => {
-    featuresDrop.classList.remove('hidden')
-})
-featuresMenu.addEventListener('mouseleave', e => {
-    featuresDrop.classList.add('hidden')
+featuresMenu.addEventListener('click', e => {
+    featuresDrop.classList.toggle('hidden')
 })
 
-companyMenu.addEventListener('mouseenter', e => {
-    companyDrop.classList.remove('hidden');
+companyMenu.addEventListener('click', e => {
+    companyDrop.classList.toggle('hidden');
 })
-companyMenu.addEventListener('mouseleave', e => {
-    companyDrop.classList.add('hidden');
+
+hamMenu.addEventListener('click', e => {
+    console.log('first')
 })
